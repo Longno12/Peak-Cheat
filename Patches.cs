04a5b2c4f1260c5a93585e967f2bd59757701d22
Longno12 <157077978+Longno12@users.noreply.github.com>
2025-09-07 17:05:27 +0100

    Update Patches.cs

--- Patches.cs
+++ Patches.cs
@@ -1,15 +1,172 @@
-﻿using System;
+using HarmonyLib;
+using MyCoolMod;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using static Character;
 
 namespace ClassLibrary1
 {
     internal class Patches
     {
+        [HarmonyPatch(typeof(CharacterMovement), "GetMovementForce")]
+        [HarmonyPostfix]
+        public static void SpeedHackPatch(ref float __result)
+        {
+            if (Plugin.SpeedMultiplier > 1.0f)
+            {
+                __result *= Plugin.SpeedMultiplier;
+            }
+        }
 
-        // SOON
+        [HarmonyPatch(typeof(CharacterMovement), "JumpRpc")]
+        [HarmonyPrefix]
+        public static void JumpHackPatch(CharacterMovement __instance)
+        {
+            if (Plugin.JumpMultiplier > 1.0f)
+            {
+                __instance.jumpImpulse *= Plugin.JumpMultiplier;
+                __instance.StartCoroutine(ResetJumpImpulse(__instance));
+            }
+        }
 
+        private static System.Collections.IEnumerator ResetJumpImpulse(CharacterMovement movementInstance)
+        {
+            yield return new WaitForEndOfFrame();
+            if (Plugin.JumpMultiplier > 1.0f)
+            {
+                movementInstance.jumpImpulse /= Plugin.JumpMultiplier;
+            }
+        }
+
+        [HarmonyPatch(typeof(CharacterMovement), "CheckFallDamage")]
+        [HarmonyPrefix]
+        public static bool NoFallDamagePatch()
+        {
+            if (Plugin.NoFallDamageEnabled)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(Character), "UseStamina")]
+        [HarmonyPrefix]
+        public static bool AlwaysSprintPatch(Character __instance, float usage)
+        {
+            if (Plugin.AlwaysSprintEnabled && __instance.data.isSprinting)
+            {
+                return false;
+            }
+            return true;
+        }
+        [HarmonyPatch]
+        public static class SprintPatches
+        {
+            private static MethodInfo checkSprintMethod;
+
+            [HarmonyPrepare]
+            public static bool Prepare()
+            {
+                checkSprintMethod = AccessTools.Method(typeof(Character), "CheckSprint");
+                if (checkSprintMethod == null)
+                {
+                    Plugin.Log.LogError("Could not find internal method Character.CheckSprint(). The Always Sprint patch will be disabled.");
+                    return false;
+                }
+
+                Plugin.Log.LogInfo("Successfully found internal method Character.CheckSprint() for sprint patch.");
+                return true;
+            }
+
+            [HarmonyPatch(typeof(Character), "FixedUpdate")]
+            [HarmonyPostfix]
+            public static void ForceSprintPatch(Character __instance)
+            {
+                if (Plugin.AlwaysSprintEnabled && __instance.IsLocal)
+                {
+                    bool canSprint = (bool)checkSprintMethod.Invoke(__instance, null);
+                    if (canSprint)
+                    {
+                        __instance.data.isSprinting = true;
+                        __instance.data.sinceUseStamina = 10f;
+                    }
+                }
+            }
+
+            [HarmonyPatch(typeof(Character), "UseStamina")]
+            [HarmonyPrefix]
+            public static bool NoStaminaForSprintPatch(Character __instance)
+            {
+                if (Plugin.AlwaysSprintEnabled && __instance.data.isSprinting)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+        [HarmonyPatch(typeof(CharacterItems), "DropAllItems")]
+        public static class KeepItemsPatch
+        {
+            [HarmonyPrefix]
+            public static bool Prefix()
+            {
+                if (Plugin.KeepItemsEnabled)
+                {
+                    Plugin.Log.LogInfo("KeepItems is enabled, preventing items from dropping on death.");
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(GameOverHandler), "BeginIslandLoadRPC")]
+        public class SoftLockPatch
+        {
+            private static bool Prefix()
+            {
+                return !ModGUI.NoSL;
+            }
+        }
+
+        [HarmonyPatch(typeof(Character), "RPCA_PassOut")]
+        public class PassOutPatch
+        {
+            private static bool Prefix()
+            {
+                return !ModGUI.NoP;
+            }
+        }
+
+        [HarmonyPatch(typeof(Character), "PassOut")]
+        public class PassOutPatch1
+        {
+            private static bool Prefix()
+            {
+                return !ModGUI.NoP;
+            }
+        }
+
+        [HarmonyPatch(typeof(Character), "RPCA_Die")]
+        public class DiePatch1
+        {
+            private static bool Prefix()
+            {
+                return !ModGUI.NoD;
+            }
+        }
+
+        [HarmonyPatch(typeof(Character), "Die")]
+        public class DiePatch2
+        {
+            private static bool Prefix()
+            {
+                return !ModGUI.NoD;
+            }
+        }
     }
 }
