904ee671fc6e660a2e0a8d6f950320130481e84b
Longno12 <157077978+Longno12@users.noreply.github.com>
2025-09-07 21:33:42 +0100

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,7 @@
     {
         public const string PLUGIN_GUID = "com.myname.peak";
         public const string PLUGIN_NAME = "Peak Mod GUI";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
     }
+
 }
